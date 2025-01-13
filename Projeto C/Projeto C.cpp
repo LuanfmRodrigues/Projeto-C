40575abf79a20768958595a64290bac14065ec14
Luan Rodrigues <158163656+LuanfmRodrigues@users.noreply.github.com>
2025-01-13 19:37:22 +0000

    ola

--- Projeto C/Projeto C.cpp
+++ Projeto C/Projeto C.cpp
@@ -1,4 +1,4 @@
-// Projeto C.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Projeto C.cpp : This file contains the 'main' function. Program execution begins and ends there
 //
 
 #include <iostream>
