e3343b5ca5774c167efd17b86fed07b786425bb8
Tarik Loureiro <Tarik.Loureiro.T0130439@edu.atec.pt>
2025-01-20 23:02:08 +0000

    adicionar existente e clear de tela

--- Projeto C/Projeto C.cpp
+++ Projeto C/Projeto C.cpp
@@ -4,53 +4,87 @@
 #include <iostream>
 #include <locale>
 #include <string>
+#include <cstdlib>
 using namespace std;
-// Maximo de produtos no estoque podemos vem outra forma de limitar.
-const int MaxProdutos = 60;
-
-void produtos()
+void produtos(int &qtd, string &id, string **mat, string &nome, double &preco, int &quantidade, int &contador)
 {
-    //id, nome, quantidade,preco,qtd
+    system("cls");
+
     
 
 }
-void adicionarex()
+void adicionarEx(int& qtd, string& id, string** mat, string& nome, double& preco, int& quantidade, int& contador)
 {
+    system("cls");
+
+    int valorid;
+    int acrescento;
+    cout << "Id do produto que quer acrescentar: ";
+    cin >> valorid;
+    if (valorid > contador && valorid < 1)
+    {
+        cout << "\nId introduzido invalido ou nao existente";
+    }else
+    {
+        valorid=valorid - 1;
+        for (int j = 0; j < 4; j++)
+        {
+            cout << mat[valorid][j] << "\t";
+        }
+        cout << "\nIntroduzir a quantidade do produto que deseja acrescentar: ";
+        cin >> acrescento;
+        mat[valorid][2] = to_string(stoi(mat[valorid][2]) + acrescento);
+        cout << "\nNova quantidade: " << mat[valorid][2];
+        
+    }
 }
-void adicionarnovo(int& qtd, string& id, string** mat, string& nome, double& preco, int& quantidade, int &contador)
-{  
-    contador = 0;
+void adicionarNovo(int &qtd, string &id, string **mat, string &nome, double &preco, int &quantidade, int &contador)
+{
+    system("cls");
+
+    int contadorinterno = 0;//para quando se refizer a ação nao mostrar os itens todos que foram adicionados
     char choice4;
     do{  
-        contador++;
-        mat[contador][0]=to_string(contador);
+        contadorinterno++;
+        mat[contador][0]=to_string(contadorinterno);
         cout << "Introduzir o nome: ";
         cin >> mat[contador][1];
         cout << "Introduzir a quantidade: ";
         cin >> mat[contador][2];
         cout << "Introduzir o preco: ";
         cin >> mat[contador][3];
-        cout << "Deseja adicionar mais produtos?.. (s/n)";
+        contador++;//para depois representar o numero de itens totais no inventario
+        if (contador>qtd)
+        {
+            qtd++;
+        }
+        cout << "Deseja adicionar mais produtos?.. (s/n)\n";
         cin >> choice4;
             
         
     }while (choice4 == 's' || choice4 == 'S');
-    for (int i = 0; i < contador+1; i++)
+    for (int i = 0; i < contadorinterno; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            cout << mat[i][j]<< "\t";
+            cout << mat[i][j]<< "\t|";
         }
         cout << "\n";
     }
 }
 
 
-void remover(){}
+void remover()
+{
+    system("cls");
+
+}
 
 //Lembrar que tem que declarar os ponteiros nos parametros para poder pecorre entre funcoes.  
 void adicionar(int &qtd,string &id, string **mat, string &nome, double &preco, int &quantidade, int &contador)
 {
+    system("cls");
+
     int choice3;
     do
     {
@@ -60,11 +94,11 @@ void adicionar(int &qtd,string &id, string **mat, string &nome, double &preco, i
         {
         case 1:
             //Adicionar a um produto ja existente
-            adicionarex();
+            adicionarEx(qtd, id, mat, nome, preco, quantidade, contador);
             break;
         case 2:
             
-            adicionarnovo(qtd, id, mat, nome, preco, quantidade, contador);
+            adicionarNovo(qtd, id, mat, nome, preco, quantidade, contador);
             break;
         case 3:
             break;
@@ -79,29 +113,10 @@ void adicionar(int &qtd,string &id, string **mat, string &nome, double &preco, i
 
 
 }
-void Stock()
+void Stock(int& qtd, string& id, string** mat, string& nome, double& preco, int& quantidade, int& contador)
 {
-    //Tamanho dos ponteiros de quantidade
-    int qtd = 100;
-    int contador = 0;
-    string id;
-    string** mat= new string*[qtd];
-    string nome;
-    double preco;
-    int quantidade;
-    int choice1;
-    
-    for (int i = 0; i < 100; i++)
-    {
-        mat[i] = new string[qtd];
-        for (int j = 0; j < 4; j++)
-        {
-            mat[i][j] = "";
-        }
-    }
-    
-
-        
+    system("cls");
+    int choice1;  
     do
     {
         cout << " 1-Adicionar \n" << " 2-Remover \n" << " 3- Produtos \n" << " 4-Sair \n";
@@ -115,7 +130,7 @@ void Stock()
             remover();//Para remover artigos do stock existente
             break;
         case 3:
-            produtos();//Para visualizar os produtos existentes
+            produtos(qtd, id, mat, nome, preco, quantidade, contador);//Para visualizar os produtos existentes
             break;
         case 4:
             break;
@@ -125,13 +140,36 @@ void Stock()
         }
     } while (choice1 != 4);
 }
-void Carrinho() {}
-void Venda() {}
+void Carrinho()
+{
+    system("cls");
+}
+void Venda() 
+{
+    system("cls");
+}
 
 
 int main()
 {
+    //inicialização das variaveis todas
+    int qtd = 100;
+    int contador = 0;//contador de todos os produtos que se tem
+    string id;
+    string** mat = new string * [qtd];//matriz onde se guarda os valores de produtos todos
+    string nome;
+    double preco;
+    int quantidade;
 
+    //inicialização da matriz
+    for (int i = 0; i < 100; i++)
+    {
+        mat[i] = new string[qtd];
+        for (int j = 0; j < 4; j++)
+        {
+            mat[i][j] = "";
+        }
+    }
     setlocale(LC_ALL, "");
     int choice;
     do {
@@ -140,7 +178,7 @@ int main()
         switch (choice)
         {
         case 1:
-            Stock();    //Chamar função para entrar no stock, onde se localiza tudo em rela��o a adicionar remover e visualizar stock
+            Stock(qtd, id, mat, nome, preco, quantidade, contador);    //Chamar função para entrar no stock, onde se localiza tudo em rela��o a adicionar remover e visualizar stock
             break;
         case 2:
             Carrinho(); //Chamar função para o carrinho que vai permitir fazer vendas
